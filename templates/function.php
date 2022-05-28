311250255ddecdcf1b738fd11fe12ec9c0e9f50b
21tejasgupta <98201091+21tejasgupta@users.noreply.github.com>
2022-05-28 23:24:16 +0530

    Add files via upload

---
+++ templates/function.php
@@ -0,0 +1,19 @@
+    <html>
+    <?php
+
+    echo "Function called successfully";
+
+    $conn = mysqli_connect("localhost", "root", "", "webcam");
+
+    if (isset($_FILES["webcam"]["tmp_name"])) {
+        $tmpName = $_FILES["webcam"]["tmp_name"];
+        $imageName = date("Y.m.d") . "-" . date("h.i.sa") . '.jpeg';
+        move_uploaded_file($tmpName, 'img/' . $imageName);
+
+        $date = date("Y/m/d") . "&" . date("h:i:sa");
+        $query = "INSERT INTO tb_image VALUES('','$date','$imageName')";
+        mysqli_query($conn, $query);
+    }
+    ?>
+
+    </html>
